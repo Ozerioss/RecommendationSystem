8531c8d19fe42a37c83c18f5ac20c4386fd1f469
Karim <ozerioss@gmail.com>
2018-12-30 01:33:43 +0100

    Adds weight for rating

--- main.py
+++ main.py
@@ -24,15 +24,26 @@ def PopularItems():
     del movies_df['zab']
 
     movie_id_sup3 = ratings_df.loc[ratings_df['rating'] >= 3] #keep only the movies that have better than average rating
-
     movie_id_sup3_count = movie_id_sup3['movie_id'].value_counts() #count number of reviews for movies
-    
+
     result = pandas.DataFrame({'movie_id':movie_id_sup3_count.index, 'count':movie_id_sup3_count.values})
+
+    mean_rating = movie_id_sup3.groupby(['movie_id'], as_index=False)['rating'].mean()
+    #print(mean_rating['movie_id'].loc[mean_rating['movie_id'] == 2858], mean_rating['rating'])
 
     top_10_movies = result[:20] #Keep only the best 20 movies
 
+    merged_rating = pandas.merge(top_10_movies, mean_rating, on=['movie_id'], how = 'inner')
+
+    merged_rating['coefficient'] = merged_rating['rating'] * merged_rating['count']
+
+
+    merged_df_withRating = pandas.merge(merged_rating, movies_df, on=['movie_id'], how='inner')
+
+    #Merge dataframe on movie id to grav the movie title
     merged_df = pandas.merge(top_10_movies, movies_df, on=['movie_id'], how='inner')
 
+    print(merged_df_withRating['movie_title'])
     print(merged_df['movie_title'])
 
 
